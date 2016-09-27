446d12c5065537ec0ab879a789bd58acff118df7
CAD Client <teenta3027@gmail.com>
2016-09-27 17:18:40 +0900

    added lab 01 intro/02_output_format.py

---
+++ lap 01 intro/02_output_format.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+import math
+
+print ("1234567890" * 4)
+print ("%d" % (math.pi))
+print ("%5d" % (math.pi))
+print ("%5f" % (math.pi))
+print ("%8f" % (math.pi))
+print ("%+8f" % (math.pi))
+
+print ("%16.8f" % (math.pi))
+print ("1234567890" * 4)
+
+# https://docs.python.org/2.7/library/stdtypes.html#string-formatting
