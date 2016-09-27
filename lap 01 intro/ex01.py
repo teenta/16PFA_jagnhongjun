e3fdb5fb201918f9f8f3058d611eabadbf63ccac
CAD Client <teenta3027@gmail.com>
2016-09-27 17:16:25 +0900

    added lab 01 intro/ex01.py

---
+++ lap 01 intro/ex01.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+# 위 주석은 이 .py 파일 안에 한글이 사용되었다는 점을 표시하는 것임
+# 각 문자열을 화면에 표시
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+# Python 3.x 인 경우 문자열을 () 로 감싸주면 됨. 이겄은 Python 2.x 에서도 작동함.
+print ("This is fun.")
+print ('Yay! Printing')
+print ("I'd much rather you'not'.")
+print ('I "said" do not touch this.')
