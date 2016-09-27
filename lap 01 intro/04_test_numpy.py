3759416402dc2843efaafb38634a904db37b82c6
CAD Client <teenta3027@gmail.com>
2016-09-27 17:19:28 +0900

    added lab 01 intro/04_test_numpy.py

---
+++ lap 01 intro/04_test_numpy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+# 배열, 행렬 관련 기능을 담고 있는 numpy 모듈을 불러 들여 np 라는 이름 아래 연결함
+import numpy as np
+
+r1 = np.array((1.0, 2.0))
+print "r1 =", r1
+
+r2 = np.array((-2.0, 1.0))
+print "r2 =", r2
+
+r3 = r1 + r2
+print "r3=" , r3
+
+# Numpy Quickstart tutorial, https://docs.scipy.org/doc/numpy-dev/user/quickstart.html
